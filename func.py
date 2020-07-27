d02a0d74ececb07db4d6b4517921682267d52983
sakshi9401 <68016527+sakshi9401@users.noreply.github.com>
2020-07-27 20:04:55 +0530

    Add files via upload

---
+++ func.py
@@ -0,0 +1,13 @@
+#basic func
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
+#arguments in function
+def my_function(fname):
+  print(fname + " Red")
+
+my_function("apple")
+my_function("cherry")
+my_function("berry")
