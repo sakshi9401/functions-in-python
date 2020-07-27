6ea18a21119c0a0e08b819f6fbda847d6d11bb58
sakshi9401 <68016527+sakshi9401@users.noreply.github.com>
2020-07-27 20:35:31 +0530

    Update func2.py

--- func2.py
+++ func2.py
@@ -1,4 +1,13 @@
+#addition of numbers of arg
+
 def my_function(fname, lname):
     print(fname +" "+ lname)
 
 my_function("sakshi","raikwar")
+
+#if function expects 2 arg and get only one it will give error
+
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil") 
